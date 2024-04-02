2c73a367ab4cc558ba08b260aa69764f690bd1da
EddyVenus <eddyolmosv@gmail.com>
2024-04-02 13:55:51 -0500

    Se instaló node-fetch para un ejercicio

--- src/es13/00-at.js
+++ src/es13/00-at.js
@@ -1,3 +1,5 @@
-const array = ["one", "two", "three", "four", "five", "six", "seven"];
+const array = ["one", "two", "three", "four", "five", "six", "seven", "ten"];
 
-console.log(array);
+console.log(array[array.length - 1]);   // Manera convencional
+
+console.log(array.at(-1));  // .at() para obtener los elementos según el índice del array y -1 para el último elemento del arreglo (negativos empiezan desde el último elemento)
